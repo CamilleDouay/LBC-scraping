1c540fc8ecf12d326f7fc0e73567269cb5a1c549
Camille Douay <camille.douay@gmail.com>
2016-06-14 14:01:01 +0200

    Ajout du titre, de la description, de la date de mise en ligne et du nom

--- Key_value.R
+++ Key_value.R
@@ -5,12 +5,25 @@ library(plyr)
 library(rvest)
 
 
+Unaccent <- function(text) {
+  text <- gsub("['`^~\"]", " ", text)
+  text <- iconv(text, to="ASCII//TRANSLIT//IGNORE")
+  text <- gsub("['`^~\"]", "", text)
+  return(text)
+}
+
 get_desc <- function(url_df){
   result = data.frame()
   
-  
   'span.property' -> css_key
   'span.value' -> css_value
+  
+  
+  xpath_value = '//*[contains(concat( " ", @class, " " ), concat( " ", "value", " " ))]'
+  xpath_property = '//*[contains(concat( " ", @class, " " ), concat( " ", "property", " " ))]'
+  xpath_title = '//*[contains(concat( " ", @class, " " ), concat( " ", "no-border", " " ))]'
+  xpath_photos =  '//*[contains(concat( " ", @class, " " ), concat( " ", "item_photo", " " ))]'
+  xpath_MEL = '//*[contains(concat( " ", @class, " " ), concat( " ", "line_pro", " " ))]'
   
   
   for (i in url_df[,]){
@@ -21,32 +34,59 @@ get_desc <- function(url_df){
     if (class(exc) != "try-error"){
       url %>%
         read_html() %>%
-        html_nodes(css_key)%>%
+        html_nodes(xpath = xpath_property)%>%
         html_text()%>%
         repair_encoding('utf-8')%>%
-        data.frame()-> key_page
-    
-      key_strip = apply(key_page, 2, function(x) trimws(x))
+        (function(x)gsub("[^[:alnum:][:space:]']", "", x)) %>%
+        Unaccent()%>%
+        trimws()-> property
+
+
+      url %>%
+        read_html() %>%
+        html_nodes(xpath = xpath_value)%>%
+        html_text()%>%
+        repair_encoding('utf-8')%>%
+        trimws()%>% 
+        t()%>% 
+        data.frame()-> value
+      
       
       url %>%
         read_html() %>%
-        html_nodes(css_value)%>%
+        html_nodes(xpath = xpath_title)%>%
         html_text()%>%
         repair_encoding('utf-8')%>%
-        data.frame()-> value_page
+        trimws()-> titre
       
-      value_strip = apply(value_page, 2, function(x) trimws(x))
       
-      value_strip
-    
-      df = data.frame(t(value_strip))
-      colnames(df) = t(key_strip)
-      result = rbind.fill(result, df)
+      url %>%
+        read_html() %>%
+        html_nodes(xpath = xpath_photos)%>%
+        html_text()%>%
+        repair_encoding('utf-8')%>%
+        trimws()-> photos
+      
+      url %>%
+        read_html() %>%
+        html_nodes(xpath = xpath_MEL)%>%
+        html_text()%>%
+        repair_encoding('utf-8')%>%
+        trimws()-> MEL
+
+      
+      colnames(value) = property
+      colnames(value)[is.na(colnames(value))] = 'NA'
+      value$Titre = titre
+#      value$Photos = photos
+      value$MEL = MEL[1]
+      value$User = MEL[2]
+
+      result = rbind.fill(result, value)
       }else{
       print('Adress Error')
     }
-    #print(url)
-    #print(url_split)
+
   }
   return(result)
 }
@@ -65,6 +105,7 @@ extract_LBC = function(nb_page){
     print(i)
     url_lbc = paste(lbc_url, cat,'/',search_url, '?o=',i, sep = "")
     
+
     css_page = 'a.list_item.clearfix.trackable'
     
     res <- try(read_html(url_lbc))
@@ -109,9 +150,14 @@ parse_df <- function(df){
 }
 
 
-rawdf = extract_LBC(1000)
+rawdf = extract_LBC(1)
+
+rawdf
+get_desc(rawdf)
+
 head(rawdf)
 dim(rawdf)
+
 df = parse_df(rawdf)
 
 summary(df)
